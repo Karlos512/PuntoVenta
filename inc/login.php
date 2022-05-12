0d1aba634f853ef72d39c2a903405fed7a1524cc
Carlos Herrera <solrac512@gmail.com>
2022-05-11 19:30:27 -0500

    Modificacion final Login

--- inc/login.php
+++ inc/login.php
@@ -1,15 +1,17 @@
 <body>
 
-<div class="col-md-4 col-md-offset-4 col-xs-12">
+<div class="col-md-4 col-md-offset-4 col-xs-12" id="login">
+    <center>
     <img src="img/validate.gif" alt="una imagen">
+    </center>
     <div class="col-md-12">
-        <div class="form-group">
+        <div class="form-group text-center">
             <label for="usuario">Nombre de usuario</label>
             <input placeholder="Nombre de usuario" type="text" id="usuario" class="form-control">
         </div>
     </div>
     <div class="col-md-12">
-        <div class="form-group">
+        <div class="form-group text-center">
             <label for="palabra_secreta">Contraseña</label>
             <input placeholder="Contraseña" type="password" id="palabra_secreta" class="form-control">
         </div>
@@ -24,8 +26,8 @@
 body{
   width: 100%;
   color: #ffffff;
-  background: -webkit-linear-gradient(left, #2293D6, #2463D3, #2293D6, #2463D3);
-  background: linear-gradient(to right, #2293D6, #2463D3, #2293D6, #2463D3);
+  background: -webkit-linear-gradient(left, #2293D6, #244BD3, #2293D6, #244BD3);
+  background: linear-gradient(to right, #2293D6, #244BD3, #2293D6, #244BD3);
   background-size: 600% 100%;
   -webkit-animation: HeroBG 20s ease infinite;
           animation: HeroBG 20s ease infinite;
@@ -54,6 +56,9 @@ body{
     background-position: 0 0;
   }
 }
+img{
+    width: 65%;
+}
 </style>
 
 <!--  -->
@@ -79,29 +84,18 @@ body{
                 $("#usuario").focus();
                 return;
             }
-            var html_original = $("#iniciar_sesion").html();
-            $("#iniciar_sesion")
-                .html('Comprobando... <i class="fa fa-spin fa-refresh"></i>')
-                .removeClass('btn-success btn-warning btn-danger')
-                .addClass('btn-warning');
+            
             $.post('./modulos/usuarios/comprobar_datos.php', {datos_usuario: [usuario, palabra_secreta]}, function (respuesta) {
 
                 respuesta = JSON.parse(respuesta);
-                console.log("La respuesta es:", respuesta);
                 if (respuesta === true) {
-                    $("#iniciar_sesion")
-                        .html('Correcto <i class="fa fa-check-square"></i>')
-                        .removeClass('btn-success btn-warning btn-danger')
-                        .addClass('btn-success')
-                        .animateCss("bounceOut");
                     setTimeout(function () {
                         window.location.reload();
-                    }, 200);
+                    }, 1000);
                 } else {
                     $("#iniciar_sesion")
-                        .html('Datos incorrectos <i class="fa fa-exclamation"></i>')
-                        .removeClass('btn-success btn-warning btn-danger')
-                        .addClass('btn-danger')
+                        .html('Datos incorrectos');
+                    $("#login")
                         .animateCss("shake");
                     $("#usuario").focus();
                 }
